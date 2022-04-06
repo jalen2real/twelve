2b516457c69052aa14c62f5ef482a41ca8bfa646
Smith <25jsmith@FVFZMYB5J1WK.tld>
2022-04-06 11:00:32 -0500

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-function ran() {
-    alret("Hello World!");
+function run() {
+    document.getElementById("paragraph").innerHTML = "Hello World!";
+    document.getElementById("paragraph").style.backgroundColor = "red";
+    document.getElementById("paragraph").style.color = "white";
+    document.getElementById("paragraph").style.padding = "20px";
+    randomNum();
+    userNumber();
+}
+function randomNum() {
+    let ran = Math.floor(Math.random () * 10);
+    let x =  document.getElementById("random");
+    x.innerHTML = ran;
+    x.style.backgroundColor = "purple";
+    x.style.color = "white";
+    x.style.padding = "20px";
+    x.style.textAlign = "center";
+
 }
 
-run();
+function userNumber() {
+    let user = document.getElementById("getNumber").value;
+    let y = document.getElementById("userNumber");
+    y.innerHTML = user;
+    y.style.color = "white";
+    y.style.backgroundColor = "orange";
+    y.style.padding = "20px";
+    y.style.textAlign = "center";
+}
+
+
+
+
+
 
